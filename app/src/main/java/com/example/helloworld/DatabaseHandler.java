db3c472ec1d9baac8db19c96c912449ee39f3d18
ysouaardi <yosua.ardi@ti.ukdw.ac.id>
2020-11-09 21:16:46 +0700

    - add feat: shared preference and database

---
+++ app/src/main/java/com/example/helloworld/DatabaseHandler.java
@@ -0,0 +1,37 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "UserDatabase.db";
+
+    private static final String TABLE_USER = "user";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASSWORD = "password";
+
+    public DatabaseHandler(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //Create table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_USER + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_USERNAME + " TEXT,"
+                + COLUMN_EMAIL + " TEXT," + COLUMN_PASSWORD + " TEXT" +")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,11 +15,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
     private EditText txtUser;
     private EditText txtPass;
     private Button btnLogin;
+    private Button btnSignup;
     private String user = "admin";
     private String pass = "admin";
+
+    DatabaseHandler dbhandler;
+    protected Cursor cursor;
+    public static SharedPreferences sharedPreferences;
+    public static final String myPref = "MY_PREF";
+    public static final String KEY_EMAIL = "KEY_EMAIL";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +38,62 @@ public class LoginActivity extends AppCompatActivity {
         txtUser = findViewById(R.id.txtUsername);
         txtPass = findViewById(R.id.txtPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        sharedPreferences = getSharedPreferences(myPref, Context.MODE_PRIVATE);
+        Log.d(TAG, "Tes "+sharedPreferences.getString(KEY_EMAIL, new String()));
+        if(!sharedPreferences.getString(KEY_EMAIL, new String()).equals("")){
+            Log.d(TAG, sharedPreferences.getString(KEY_EMAIL, new String()));
+            //ke home activity
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra("COBA_INTENT_EXTRA", "Percobaan");
+            startActivity(intent);
+        }
         btnLogin.setOnClickListener(new View.OnClickListener(){
              @Override
             public void onClick(View v) {
                  cekLogin();
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
     }
 
     public void cekLogin(){
-        if(txtUser.getText().toString().equals(user) && txtPass.getText().toString().equals(pass)){
+        dbhandler = new DatabaseHandler(this);
+        SQLiteDatabase db = dbhandler.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM user " +
+                        "WHERE username = '" + txtUser.getText().toString() + "'" +"AND password = '"+txtPass.getText().toString()+"'",null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(KEY_EMAIL, txtUser.getText().toString());
+            editor.commit();
             Intent intent = new Intent(this, HomeActivity.class);
             intent.putExtra("COBA_INTENT_EXTRA", "Percobaan");
             startActivity(intent);
-        }else {
+        }else{
             Toast.makeText(getApplicationContext(), "Username atau Password Anda tidak benar!", Toast.LENGTH_LONG).show();
         }
+
+//        if(txtUser.getText().toString().equals(user) && txtPass.getText().toString().equals(pass)){
+//            SharedPreferences.Editor editor = sharedPreferences.edit();
+//            editor.putString(KEY_EMAIL, txtUser.getText().toString());
+//            editor.commit();
+//            Intent intent = new Intent(this, HomeActivity.class);
+//            intent.putExtra("COBA_INTENT_EXTRA", "Percobaan");
+//            startActivity(intent);
+//        }else {
+//            Toast.makeText(getApplicationContext(), "Username atau Password Anda tidak benar!", Toast.LENGTH_LONG).show();
+//        }
+    }
+
+    public void signup(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
